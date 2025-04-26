161dc41e7b08dda8b2937ad741530348bb73cbf1
CharuKumar01 <kumarcharu83@gmail.com>
2025-04-26 22:18:20 +0530

    changes made

--- app/src/main/java/com/example/retrofit/HomeFragment.kt
+++ app/src/main/java/com/example/retrofit/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.retrofit.databinding.FragmentHomeBinding
@@ -16,6 +18,14 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         bind = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        val spinner: Spinner = bind.spinner
+        val options = listOf("Option 1", "Option 2", "Option 3")
+
+        val adapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
         return bind.root
     }
 }
